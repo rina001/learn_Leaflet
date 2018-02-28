55d06e223d1b7105e489bd66ed430dc71032b0f3
Rina Qiu <rinaqiu88@yahoo.com>
2018-02-27 21:52:41 -0800

    Add files via upload

---
+++ maps.R
@@ -0,0 +1,6 @@
+install.packages("leaflet")
+library(leaflet)
+library(maps)
+mapStates = map("state",fill = TRUE,plot = FALSE)
+leaflet(data = mapStates) %>% addTiles() %>%
+  addPolygons(fillColor = topo.colors(10,alpha = NULL),stroke = FALSE)
